44e428e0bf50c6b17ee633ce145c658a87e0e374
HenryD11703 <3680558330101@ingenieria.usac.edu.gt>
2025-01-13 17:14:46 -0600

    Enhance linked list HTTP endpoints with JSON responses; add search, get, and update graph functionalities

--- src/linkedListEndpoints.cpp
+++ src/linkedListEndpoints.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 #include "../include/linkedList.h"
 #include "../lib/httplib.h"
 
+using namespace nlohmann;
 using namespace httplib;
 using namespace std;
 
@@ -14,38 +16,82 @@ class LinkedListServer {
   void handleInsertHead(const Request& req, Response& res) {
     string value = req.get_param_value("value");
     ll.insertAtHead(stoi(value));
-    res.set_content("Valor insertado: " + value + "\n", "text/plain");
+    json response = {{"status", "success"},
+                     {"message", "Valor insertado en la cabeza"},
+                     {"value", value}};
+    res.set_content(response.dump(), "application/json");
   }
 
   void handleDeleteHead(const Request& req, Response& res) {
     ll.deleteHead();
-    res.set_content("Valor eliminado de la cabeza\n", "text/plain");
+    json response = {{"status", "success"},
+                     {"message", "Valor eliminado de la cabeza"}};
+    res.set_content(response.dump(), "application/json");
   }
 
   void handleInsertEnd(const Request& req, Response& res) {
     string value = req.get_param_value("value");
     ll.insertAtEnd(stoi(value));
-    res.set_content("Valor insertado al final: " + value + "\n", "text/plain");
+    json response = {{"status", "success"},
+                     {"message", "Valor insertado al final"},
+                     {"value", value}};
+    res.set_content(response.dump(), "application/json");
   }
 
   void handleDeleteEnd(const Request& req, Response& res) {
     ll.deleteEnd();
-    res.set_content("Valor eliminado del final\n", "text/plain");
+    json response = {{"status", "success"},
+                     {"message", "Valor eliminado del final"}};
+    res.set_content(response.dump(), "application/json");
   }
 
   void handleInsertAtIndex(const Request& req, Response& res) {
     string value = req.get_param_value("value");
     string index = req.get_header_value("index");
     ll.insertAtIndex(stoi(value), stoi(index));
-    res.set_content(
-        "Valor insertado en el indice: " + index + ". " + value + "\n",
-        "text/plain");
+    json response = {{"status", "success"},
+                     {"message", "Valor insertado en el índice especificado"},
+                     {"value", value},
+                     {"index", index}};
+    res.set_content(response.dump(), "application/json");
   }
 
-  void handleInsertAtIndex(const Request& req, Response& res) {
+  void handleDeleteAtIndex(const Request& req, Response& res) {
     string index = req.get_header_value("index");
     ll.deleteAtIndex(stoi(index));
-    res.set_content("Valor eliminado del indice: " + index + "\n",
-                    "text/plain");
+    json response = {{"status", "success"},
+                     {"message", "Valor eliminado del índice especificado"},
+                     {"index", index}};
+    res.set_content(response.dump(), "application/json");
+  }
+
+  // This function gets a value and returns a position or -1
+  void handleSearch(const Request& req, Response& res) {
+    string value = req.get_header_value("value");
+    int found = ll.search(stoi(value));
+    json response = {
+        {"status", "success"},
+        {"message", found != -1 ? "Valor encontrado" : "Valor no encontrado"},
+        {"value", value},
+        {"position", found}};
+    res.set_content(response.dump(), "application/json");
+  }
+  // This function gets an index and returns a value or -1
+  void handleGet(const Request& req, Response& res) {
+    string index = req.get_header_value("index");
+    int value = ll.get(stoi(index));
+    json response = {
+        {"status", "succes"},
+        {"message", value != -1 ? "Posicion invalida" : "Posicion correcta"},
+        {"value", value},
+        {"position", index}};
+    res.set_content(response.dump(), "application/json");
+  }
+
+  void updateGraph(const Request& req, Response& res) {
+    ll.generateGraphviz();
+    json response = {{"status", "success"},
+                     {"message", "Updated graph succesfully"}};
+    res.set_content(response.dump(), "application/json");
   }
 };
